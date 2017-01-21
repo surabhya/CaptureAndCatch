49237f2a23e06116e08e912047b2c82fb2d004a2
surabhya <surabhya.aryal@gmail.com>
2017-01-20 19:25:13 -0600

    Updated the UI to choose two images from gallery and make API call to print the text in the images to console.

---
+++ CaptureAndSolve/FirstImagePicker.swift
@@ -0,0 +1,9 @@
+//
+//  FirstImagePicker.swift
+//  CaptureAndSolve
+//
+//  Created by Aryal, Surabhya on 1/18/17.
+//  Copyright © 2017 Aryal, Surabhya. All rights reserved.
+//
+
+import Foundation
---
+++ SecondImagePicker.swift
@@ -0,0 +1,9 @@
+//
+//  SecondImagePicker.swift
+//  CaptureAndSolve
+//
+//  Created by Aryal, Surabhya on 1/18/17.
+//  Copyright © 2017 Aryal, Surabhya. All rights reserved.
+//
+
+import Foundation
